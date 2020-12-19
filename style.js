fb85699a1bc0c0911c38dea84da2db33d4e2b99b
CozyDe <74243625+CozyDe@users.noreply.github.com>
2020-12-19 06:15:16 -0800

    Add files via upload

--- style.js
+++ style.js
@@ -4,3 +4,13 @@ navDown.addEventListener("click", function changeNav(){
     bring.classList.toggle("is-active");
     navDown.classList.toggle("change");
 });
+
+ window.onscroll = function() {
+     var currentScroll = window.pageYOffset;
+     if (currentScroll > 30) {
+         document.querySelector(".nav-banner").style.height = "auto";
+     }
+      else {
+          document.querySelector(".nav-banner").style.height = "10vh";
+    }
+ }
